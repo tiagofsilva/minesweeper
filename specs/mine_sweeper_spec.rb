f2695870081ef950af1abf90da6ded6b0fc614f2
= <aeroboom@gmail.com>
2013-03-21 03:17:44 -0300

    kata solved and more specs created

--- specs/mine_sweeper_spec.rb
+++ specs/mine_sweeper_spec.rb
@@ -7,7 +7,6 @@ describe MineSweeper do
     @games = MineSweeper::start "resources/maps.txt"
   end
   
-  
   describe "#start" do
     context "given an existing file path" do
       it "loads games with correct properties" do
@@ -31,8 +30,47 @@ describe MineSweeper do
       it "raises an exception" do
         expect {MineSweeper::start "resources/am.txt"}.to raise_exception
       end
-    end
+    end  
       
   end
+  
+  describe "#load_clues" do
+    
+    let(:adj) {[["*", 1, 0, 0], [1, 1, 0, 0]]}
+    let(:game) {MineSweeper::start("resources/maps1.txt").first} 
+    let(:adj2) {[["*", "*", 1, 0, 0], [3, 3, 2, 0, 0], [1, "*", 1, 0, 0]]}
+    let(:game2) {MineSweeper::start("resources/maps2.txt").first}  
+    
+    it "responds to the method" do
+      game1 = @games.first
+      game1.should respond_to :load_clues
+    end
+    
+    it "counts number of mines in a map with only one mine " do
+      game.load_clues.should eql adj
+    end
+    
+    it "counts number of mines in a map with more than one mine" do
+      game2.load_clues.should eql adj2
+    end
+
+  end
+  
+  describe "#output_to" do
+    context "when receives a valid result" do
+      let(:games) {MineSweeper::start "resources/maps.txt"}
+      
+      it "writes an output that matches the file" do
+        MineSweeper::output_to "resources/clues_compare.txt", games
+        File.open "resources/clues.txt", 'r' do |file|
+          str1 = file.read
+          str2 = File.open("resources/clues_compare.txt", 'r').read
+          str1.should == str2 
+        end
+      end
+    end
+  end
+
+  
   
 end
--- src/mine_sweeper.rb
+++ src/mine_sweeper.rb
@@ -11,13 +11,60 @@ class MineSweeper
   end
   
   def MineSweeper.start path
-    raise Exception if !File.file? path
+    raise Exception unless File.file? path
     reader = MineReader.new path
     reader.load
   end
   
+  def MineSweeper.output_to path, games
+    File.open path, 'w' do |file|
+      games.each_with_index do |game, i|
+        file.puts "\n\n"  if game == games.last
+        file.puts "Field ##{i+1}:"
+        clues = game.load_clues
+        clues.collect do |row|
+          row_str = row.inject("") {|result, elem| result + elem.to_s}
+          file.write row_str + ((row != clues.last)? "\n" : "") 
+        end
+        
+      end  
+    end
+    
+  end
+  
   def write_fields
     @map.inject([]) {|result, elem| result + elem}
+  end
+  
+  def load_clues
+    @clues = @map.dup.flatten.map {|str| str.split("").map {|char| char == "."? char = 0 : char}}
+    @clues.each_with_index do |row, i|
+      row.each_with_index do |char, j|
+        if char == "*"
+          count_adjacent_mines i, j 
+        end
+      end
+    end
+    
+    return @clues
+  end
+
+  private
+  def count_adjacent_mines row, col
+    adjacencies = [[row-1, col], [row-1,col+1], [row, col+1], [row+1, col+1], [row+1, col], [row+1, col-1], [row, col-1], [row-1, col-1]] 
+    adjacencies.each do |adj|
+      if inbounds_map? adj
+        x, y = adj[0], adj[1]
+        unless @clues[x][y] == "*"
+          @clues[x][y] == "0" ? @clues[x][y] = 1 : @clues[x][y] += 1
+        end  
+      end  
+    end
+  end
+  
+  
+  def inbounds_map? point
+    return point[0] >= 0 && point[0] < @rows && point[1] >= 0 && point[1] < @cols
   end
   
 end
